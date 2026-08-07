4ee72b056abaada0da8eb7cfb04410361e2ec2f9
sadouninc <sadouninc@gmail.com>
2026-08-07 19:31:12 +0900

    feat: export market provider

--- scripts/morning_dataset/providers/__init__.py
+++ scripts/morning_dataset/providers/__init__.py
@@ -1,11 +1,15 @@
 from .base import MorningSourceProvider, ProviderResult
 from .json_file import JsonFileProvider
+from .market import MARKET_SYMBOLS, MarketProvider, fetch_yahoo_chart
 from .registry import EXPECTED_SOURCES, collect_providers, dataset_inputs
 
 __all__ = [
     "MorningSourceProvider",
     "ProviderResult",
     "JsonFileProvider",
+    "MarketProvider",
+    "MARKET_SYMBOLS",
+    "fetch_yahoo_chart",
     "EXPECTED_SOURCES",
     "collect_providers",
     "dataset_inputs",
