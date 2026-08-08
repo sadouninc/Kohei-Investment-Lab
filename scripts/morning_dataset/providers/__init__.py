3f7ec71aff214a25a750eae1be3c2fa651e5d2eb
sadouninc <sadouninc@gmail.com>
2026-08-08 10:42:37 +0900

    Export Capital Provider

--- scripts/morning_dataset/providers/__init__.py
+++ scripts/morning_dataset/providers/__init__.py
@@ -1,4 +1,5 @@
 from .base import MorningSourceProvider, ProviderResult
+from .capital import CapitalProvider
 from .json_file import JsonFileProvider
 from .market import MARKET_SYMBOLS, MarketProvider, fetch_yahoo_chart
 from .portfolio import PortfolioProvider
@@ -7,6 +8,7 @@ from .registry import EXPECTED_SOURCES, collect_providers, dataset_inputs
 __all__ = [
     "MorningSourceProvider",
     "ProviderResult",
+    "CapitalProvider",
     "JsonFileProvider",
     "MarketProvider",
     "MARKET_SYMBOLS",
